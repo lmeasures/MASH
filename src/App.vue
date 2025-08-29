d937d3818930315f18663d5cd5c60197434fe607
iSoul95 <lmeasures95@gmail.com>
2025-08-30 00:46:20 +0100

    scaffolded the basic framework for the app. Base landing page functionality is done Pulled the landing page specific stuff out to a separate file. The way that custom props/events are handled was really unintuitive to start with, but a bit of light reading around it has cleared it right up.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,84 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
+import { ref, type Ref } from 'vue';
+import type { CharacterBooleanMap, CurrentPage } from './types';
+import Landing from './pages/landing/landing.vue';
+
+type TitleState = CharacterBooleanMap<typeof titleCharacters>
+
+const currentPage: Ref<CurrentPage> = ref("landing")
+
+const titleCharacters = "MASH"
+
+/** Instantiate a blank TitleState object */
+const titleState: TitleState = {}
+/** 
+ * Loop through the characters in titleCharacters, 
+ * applying a 'default' false value to each character 
+ */
+titleCharacters.split("").forEach(char => titleState[char] = false)
+
+
 </script>
 
 <template>
-  <div class="mash-container">
-    <div class="mash-title">
+  <div id="mash-container">
+
+    <div 
+      id="mash-title"
+      :class="`position-${currentPage}`"
+    >
+      <component 
+        class="mash-title-char"
+        v-for="(char, index) in Object.keys(titleState)" :key="index"
+        :is="titleState[char] ? 's' : 'span'"
+      >
+        {{ char }}
+      </component>
     </div>
+
+    <Landing 
+      :currentPage="currentPage" 
+      @update:currentPage="val => currentPage = val" 
+    />
+
+    <div id="category-selecting">
+    </div>
+
+    <div id="magic-number-selecting">
+    </div>
+
+    <div id="selection-summary">
+    </div>
+
+    <div id="result">
+    </div>
+
   </div>
 </template>
 
 <style scoped>
+#mash-title {
+  position:absolute;
+  transform:translate(-50%, -50%);
+  transition: top 0.5s 0.4s ease;
+  
+  left:50%;
+  font-size: 3em;
+  &.position-landing {
+    top:35%;
+  }
+  &.position-category-selecting {
+    top:10%;
+  }
+  &.position-magic-number-selecting {
+    top:45%;
+  }
+  &.position-selection-summary {
+    top:45%;
+  }
+  &.position-result {
+    top:45%;
+  }
+}
 
 </style>
---
+++ src/pages/landing/landing.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { CurrentPage } from '../../types';
+
+defineProps<{
+    currentPage: CurrentPage
+}>()
+defineEmits<{
+    (e: 'update:currentPage', value: CurrentPage): void
+}>()
+
+</script>
+
+<template>
+    <transition name="fade">
+      <div id="landing" v-if="currentPage === 'landing'">
+        <button 
+          id="start-button"
+          v-on:click="$emit('update:currentPage', 'category-selecting')"
+        >
+          Start
+        </button>
+      </div>
+    </transition>
+</template>
+
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
